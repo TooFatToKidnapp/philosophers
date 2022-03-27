633d8aa79d6df66ae8f60fe50c4bdf14929fd955
Ali  Abdou <aabdou@e2r1p5.1337.ma>
2022-03-27 21:57:17 +0000

    Day 2

--- philo/philosophers.c
+++ philo/philosophers.c
@@ -6,28 +6,56 @@
 /*   By: aabdou <aabdou@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/26 17:16:55 by aabdou            #+#    #+#             */
-/*   Updated: 2022/03/26 22:28:22 by aabdou           ###   ########.fr       */
+/*   Updated: 2022/03/27 19:50:03 by aabdou           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include"philosophers.h"
 
+int init_mutex(t_all *info)
+{
+    int j;
+    
+    j = 0;
+    info->lock = malloc(sizeof(t_mutex));
+    if(!info->lock)
+        return(put_err("Error : memory allocation\n", false));
+    info->lock->forks = malloc(sizeof(pthread_mutex_t) * info->data->nb_of_philo);
+	if(!info->lock->forks)
+        return(put_err("Error : memory allocation\n", false));
+	if(pthread_mutex_init((pthread_mutex_t *)&info->lock->output, NULL) != 0)
+		return(put_err("Error : Mutex not initialized", false));
+	while(j < info->data->nb_of_philo)
+	{
+		if(pthread_mutex_init(&info->lock->forks[j], NULL) != 0)
+			return(put_err("Error : Mutex not initialized", false));
+		j++;
+	}
+	return (0);
+}
+
 int main(int ac, char**av)
 {
-    t_philo data;
+    t_all   info;
     
+	t_data data;
+    
+    info.data = &data;
     if(ac < 5 || ac > 6)
         return(put_err("Error\n", true));
     if(!parsing_arg(av))
         return(put_err("Error\n", true));
-    data.nb_of_philo = ft_atoi(av[1]);
-    data.time_to_die = ft_atoi(av[2]);
-    data.time_to_eat = ft_atoi(av[3]);
-    data.time_to_sleep = ft_atoi(av[4]);
+    info.data->nb_of_philo = ft_atoi(av[1]);
+    info.data->time_to_die = ft_atoi(av[2]);
+    info.data->time_to_eat = ft_atoi(av[3]);
+    info.data->time_to_sleep = ft_atoi(av[4]);
+	info.data->nb_of_meals = -1;
     if(av[5] != NULL)
-        data.nb_of_meals = ft_atoi(av[5]);
+        info.data->nb_of_meals = ft_atoi(av[5]);
+	if(init_mutex(&info))
+		return(0);	
 
-   printf("%d  %d  %d   %d  %d\n", data.nb_of_philo ,data.time_to_die ,data.time_to_eat ,
-         data.time_to_sleep,data.nb_of_meals );
+    printf("%d  %d  %d   %d  %d\n", info.data->nb_of_philo ,info.data->time_to_die ,info.data->time_to_eat ,
+         info.data->time_to_sleep,info.data->nb_of_meals );
     
 }
--- philo/philosophers.h
+++ philo/philosophers.h
@@ -6,7 +6,7 @@
 /*   By: aabdou <aabdou@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/26 17:11:26 by aabdou            #+#    #+#             */
-/*   Updated: 2022/03/26 22:18:40 by aabdou           ###   ########.fr       */
+/*   Updated: 2022/03/27 19:44:58 by aabdou           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,24 +18,48 @@
 #include<unistd.h>
 #include<stdlib.h>
 #include<stdbool.h>
-typedef struct lock
+
+typedef struct mutex
 {
     pthread_mutex_t *forks;
-} t_lock;
+    pthread_mutex_t *output;
+} t_mutex;
 
-typedef struct args
+typedef struct data
 {
     int nb_of_philo;
 	int time_to_die;
 	int time_to_eat;	
 	int time_to_sleep;
 	int nb_of_meals;
-	t_lock fork;
-}   t_philo;
+	t_mutex fork;
+}   t_data;
+
+typedef struct philo
+{
+    int	philo_id;
+	int eat_count;
+	long start_time;
+	long last_meal;
+	t_data *args;
+	t_mutex *mutex;
+	pthread_t thread;
+	pthread_mutex_t right_fork;
+	pthread_mutex_t left_fork;
+}	t_philo;
+
+typedef struct all
+{
+	pthread_t death;
+	t_mutex *lock;
+	t_philo *philo;
+	t_data	*data;
+}	t_all;
 
 int	ft_atoi(const char *str);
 int	ft_isdigit(int c);
 int put_err(char *str, bool i);
 int parsing_arg(char **av);
+int init_mutex(t_all *info);
 
 #endif
